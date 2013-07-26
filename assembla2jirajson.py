3087ce2fc18cce8a4aef528386f6b170e46640fb
cypx <cyprien.d@gmail.com>
2013-07-26 18:35:49 +0200

    basic issues import

--- assembla2jirajson.py
+++ assembla2jirajson.py
@@ -3,6 +3,7 @@
 
 import sys
 import json
+import re
 
 data_input = []
 data_output = []
@@ -14,37 +15,41 @@ input_field = ['users, ','spaces, ','milestones, ','ticket_statuses, ','tickets,
 input_dict = {}
 multiple_input ={}
 
+#Assembla export file is not a regular json first we serialize data we want to use
+
+#Initiate a dict key for each input type we want to save
 for s in input_field:
 	input_dict[s] = ''
-	multiple_input[s] = False
 
-
+#read input file and save wanted line into destination key 
 with open(file_input) as f:
-
 	for line in f:
-
 		for s in input_field:
 			if line.startswith(s):
-				if multiple_input[s]:
+				if input_dict[s]!='':
 					input_dict[s] += ','+line[len(s):-1]
 				else:
 					input_dict[s] += line[len(s):-1]
-					multiple_input[s] = True
 
-
+#Convert saved line into standard json data
 for s in input_field:
+	#data_input.append(json.loads('{"'+s[:-2]+'": ['+json.dumps(input_dict[s])+']}'))
 	data_input.append(json.loads('{"'+s[:-2]+'": ['+input_dict[s]+']}'))
+
+#Convert input string according to JSON encoding
+
+
+#Now we can convert JSON data according to JIRA JSON schema
 
 users_output = ''
 for i, element in enumerate(data_input[0]["users"]):
-	users_output += '{"name":"'+element["login"]+'","fullname": "'+element["fullname"]+'"}'
+	users_output += '{"name":"'+element["login"]+'","fullname": '+json.dumps(element["fullname"])+'}'
 	if i < len(data_input[0]["users"])-1:
 		users_output += ','
 
-milestones_output = {}
+versions_output = {}
 for element in data_input[2]["milestones"]:
 	space_id=element["space_id"]
-	print "space_id: "+space_id
 	if element["is_completed"]==1:
 		released='true'
 	else:
@@ -53,26 +58,50 @@ for element in data_input[2]["milestones"]:
 		releaseDate=''
 	else:
 		releaseDate=str(element["due_date"])+'T00:00:00+00:00'
-	if space_id not in milestones_output:
-		milestones_output[space_id] =  '{"name":"'+element["title"]+'","released":'+released+',"releaseDate":"'+releaseDate+'"}'
+	if space_id not in versions_output:
+		versions_output[space_id] =  '{"name":'+json.dumps(element["title"])+',"released":'+released+',"releaseDate":"'+releaseDate+'"}'
 	else:
-		milestones_output[space_id] +=  ',{"name":"'+element["title"]+'","released":'+released+',"releaseDate":"'+releaseDate+'"}'
+		versions_output[space_id] +=  ',{"name":'+json.dumps(element["title"])+',"released":'+released+',"releaseDate":"'+releaseDate+'"}'
+
+def reporter_login(id,input_dict):
+	name=""
+	for element in input_dict[0]["users"]:
+		if element["id"] == id:
+			name=element["login"]
+	return name
 
 
-	
+issues_output = {}
+for element in data_input[4]["tickets"]:
+	space_id=element["space_id"]
+	if space_id not in issues_output:
+		issues_output[space_id] = ''
+	else:
+		issues_output[space_id] += ','
+	issues_output[space_id] += '{"summary":'+json.dumps(element["summary"])+','
+	issues_output[space_id] += '"description":'+json.dumps(element["description"])+','
+	issues_output[space_id] += '"reporter":'+json.dumps(reporter_login(element["reporter_id"],data_input))+','
+	issues_output[space_id] += '"assignee":'+json.dumps(reporter_login(element["assigned_to_id"],data_input))+','
+	issues_output[space_id] += '"created":"'+element["created_on"]+'",'
+	if element["updated_at"] is not None:
+		issues_output[space_id] += '"updated":"'+element["updated_at"]+'",'
+	if element["completed_date"] is not None:
+		issues_output[space_id] += '"resolutionDate":"'+element["completed_date"]+'",'
+	issues_output[space_id] += '"externalId":"'+str(element["number"])+'"}'
 
 project_output = ''
 for i, element in enumerate(data_input[1]["spaces"]):
-	project_output += '{"name":"'+element["name"]+'",'
+	project_output += '{"name":'+json.dumps(element["name"])+','
 	project_output += '"key":"'+element["name"][0:3]+'",'
 	if element["description"] != "":
-		project_output += '"description":"'+element["description"]+'",'
-	project_output += '"versions":['+milestones_output[element["id"]]+'],"components": ["Component","AnotherComponent"]}'
+		project_output += '"description":'+json.dumps(element["description"])+','
+	project_output += '"versions":['+versions_output[element["id"]]+'],'
+	project_output += '"issues":['+issues_output[element["id"]]+'],'
+	project_output += '"components": ["Component","AnotherComponent"]}'
 	if i < len(data_input[1]["spaces"])-1:
 		project_output += ','
 
 data_output.append(json.loads('{"users": ['+users_output+'],"projects": ['+project_output+']}'))
-
 
 with open(file_output, 'wb') as f:
 	f.write((json.dumps(data_output))[1:-1])
