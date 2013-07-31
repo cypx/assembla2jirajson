c615e1bc780476ae3de66d0aa92ab0242ea98646
cypx <cyprien.d@gmail.com>
2013-07-31 13:31:34 +0200

    add attachment import

--- assembla2jirajson.py
+++ assembla2jirajson.py
@@ -10,8 +10,32 @@ data_output = []
 
 file_input = sys.argv[1]
 file_output = sys.argv[2]
+attachement_url = sys.argv[3]
 
-input_field = ['users, ','spaces, ','milestones, ','ticket_statuses, ','tickets, ','estimate_histories, ','user_tasks, ','ticket_comments, ','ticket_associations, ']
+workflow_conversion = {
+	"New":"Open",
+	"In progress":"In Progress",
+	"Invalid":"Closed",
+	"Fixed":"Resolved",
+	"To test":"In Progress",
+	"Waiting information":"Open",
+	"To implement":"Open",
+	"Duplicate":"Closed",
+	"Test pending":"Open",
+	"Waiting subtask treatment":"Open",
+	"Waiting deploy":"Open" }
+
+input_field = ['users, ',
+	'spaces, ',
+	'milestones, ',
+	'ticket_statuses, ',
+	'tickets, ',
+	'estimate_histories, ',
+	'user_tasks, ',
+	'ticket_comments, ',
+	'ticket_associations, ',
+	'documents, ']
+
 input_dict = {}
 
 #Assembla export file is not a regular json first we serialize data we want to use
@@ -52,9 +76,27 @@ def ticket_milestone(id,input_dict):
 			milestone=element["title"]
 	return milestone
 
-def ticket_status(id):
-	ticket_statuses={5400083:"Open",5400093:"In Progress",5400103:"Closed",5400113:"Closed",5400123:"In Progress",6047193:"Open",6047203:"Open",6047213:"Closed",8325293:"Open",8353403:"Open",8618813:"Open" }
-	return ticket_statuses[id]
+def space_key (id):
+	key=""
+	for element in data_input[1]["spaces"]:
+		if element["id"] == id:
+			key=element["name"]
+	return key
+
+# we user "workflow_conversion" parameter for mapping assembla workflow to jira workflow
+# if a status is not present in "workflow_conversion" we use internal assembla state
+def ticket_status(id,input_dict):
+	jira_name=""
+	for element in input_dict[3]["ticket_statuses"]:
+		if element["id"] == id:
+			if element["name"] in workflow_conversion:
+				jira_name = workflow_conversion[element["name"]]
+			else:
+				if element["state"] == 1:
+					jira_name = "Open"
+				else:
+					jira_name = "Closed"
+	return jira_name
 
 def ticket_priority(id):
 	ticket_priorities={1:"Blocker",2:"Critical",3:"Major",4:"Minor",5:"Trivial"}
@@ -96,6 +138,20 @@ for element in data_input[7]["ticket_comments"]:
 		comments_output[ticket_id] += '"body":'+json.dumps(element["comment"])+','
 		comments_output[ticket_id] += '"created":"'+element["created_on"]+'"}'
 
+attachments_output = {}
+for element in data_input[9]["documents"]:
+	ticket_id=element["ticket_id"]
+	if ticket_id not in attachments_output:
+		attachments_output[ticket_id] = ''
+	else:
+		attachments_output[ticket_id] += ','
+	attachments_output[ticket_id] += '{"name" :'+json.dumps(element["name"])+','
+	attachments_output[ticket_id] += '"attacher":'+json.dumps(reporter_login(element["created_by"],data_input))+','
+	attachments_output[ticket_id] += '"created":'+json.dumps(element["created_at"])+','
+	if element["description"] is not None:
+		attachments_output[ticket_id] += '"description":'+json.dumps(element["description"])+','
+	attachments_output[ticket_id] += '"uri":"'+attachement_url+element["id"]+'_1"}'
+
 issues_output = {}
 for element in data_input[4]["tickets"]:
 	space_id=element["space_id"]
@@ -105,7 +161,7 @@ for element in data_input[4]["tickets"]:
 		issues_output[space_id] += ','
 	issues_output[space_id] += '{"summary":'+json.dumps(element["summary"])+','
 	issues_output[space_id] += '"description":'+json.dumps(element["description"])+','
-	issues_output[space_id] += '"status":'+json.dumps(ticket_status(element["ticket_status_id"]))+','
+	issues_output[space_id] += '"status":'+json.dumps(ticket_status(element["ticket_status_id"],data_input))+','
 	issues_output[space_id] += '"reporter":'+json.dumps(reporter_login(element["reporter_id"],data_input))+','
 	issues_output[space_id] += '"assignee":'+json.dumps(reporter_login(element["assigned_to_id"],data_input))+','
 	issues_output[space_id] += '"created":"'+element["created_on"]+'",'
@@ -120,6 +176,9 @@ for element in data_input[4]["tickets"]:
 		issues_output[space_id] += '"comments":['+comments_output[element["id"]]+'],'
 	if element["priority"] is not None:
 		issues_output[space_id] += '"priority":'+json.dumps(ticket_priority(element["priority"]))+','
+	if element["id"] in attachments_output:
+		issues_output[space_id] += '"attachments":['+attachments_output[element["id"]]+'],'
+	issues_output[space_id] += '"key":"'+space_key(space_id)+'-'+str(element["number"])+'",'
 	issues_output[space_id] += '"externalId":"'+str(element["number"])+'"}'
 
 project_output = ''
